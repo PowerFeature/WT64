c189b6dbea175863dcb9a9bb680942520518c67a
Jens Schwarzer <jeschwar@microsoft.com>
2020-04-09 12:05:28 +0200

    Fixed incorrect memory reporting

--- reset.ps1
+++ reset.ps1
@@ -1,7 +1,7 @@
 "LOADING PLEASE WAIT ..."
 $e = "$([char]27)"
-$mem = [regex]::Match((wmic OS get TotalVisibleMemorySize /Value),'[0-9]+').Value + '000'
-$free = [regex]::Match((wmic OS get FreePhysicalMemory /Value),'[0-9]+').Value + '000'
+$mem = [regex]::Match((wmic OS get TotalVisibleMemorySize /Value),'[0-9]+').Value + ''
+$free = [regex]::Match((wmic OS get FreePhysicalMemory /Value),'[0-9]+').Value + ''
 
 # This will display READY.\n as prompt
 # function prompt {"READY.`n"}
